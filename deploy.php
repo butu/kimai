3a80d730552d76995a48137614a954f58d213e1d
Benjamin Butschell <bb@webprofil.at>
2026-03-04 16:37:58 +0100

    [TASK] Run migrations during deployment

--- deploy.php
+++ deploy.php
@@ -21,4 +21,10 @@ try {
 
 // Hooks
 
+desc('Run database migrations');
+task('database:migrate', function () {
+    run('{{bin/console}} doctrine:migrations:migrate --no-interaction --allow-no-migration');
+});
+
+after('deploy:vendors', 'database:migrate');
 after('deploy:failed', 'deploy:unlock');
